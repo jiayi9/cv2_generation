47f60fc64a987820e54994d55d5dfae15ef720a5
Jiayi Lu <215756347@qq.com>
2019-09-06 13:59:05 +0800

    Update generate_random_images.py

--- generate_random_images.py
+++ generate_random_images.py
@@ -4,7 +4,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 import uuid
 
-def generate_random_images(mode, output_folder, N, length = None, length_std= None, scale = (200,200), thickness = 2, ratio = 0.01, border_ratio = (0.2, 0.8)):
+def generate_random_images(mode, output_folder, N, 
+                           length_mean = None, length_std= None, thickness = 2, 
+                           radius_mean = None, radius_std = None, 
+                           scale = (200,200), ratio = 0.01, border_ratio = (0.2, 0.8)):
     for i in range(N):
         img = np.random.binomial(1, ratio, scale).astype(float)
         
@@ -13,33 +16,57 @@ def generate_random_images(mode, output_folder, N, length = None, length_std= No
                             np.random.uniform(scale[0]*border_ratio[0], scale[0]*border_ratio[1], 1).astype(int),
                             np.random.uniform(scale[0]*border_ratio[0], scale[1]*border_ratio[1], 1).astype(int)
                             )
-            if length == None:
-                length = scale[0]/10
+            if length_mean == None:
+                length_mean = scale[0]/10
             if length_std == None:
                 length_std = scale[0]/100
-            ending_point = tuple(starting_point  + length*np.random.normal(0, length_std, 2).astype(int))
+            ending_point = tuple(starting_point  + length_mean*np.random.normal(0, length_std, 2).astype(int))
             starting_point = tuple(starting_point)
             new = cv2.line(img, starting_point, ending_point, color = 1, thickness = thickness)
+        if mode == "circle":
+            center = np.append(
+                            np.random.uniform(scale[0]*border_ratio[0], scale[0]*border_ratio[1], 1).astype(int),
+                            np.random.uniform(scale[0]*border_ratio[0], scale[1]*border_ratio[1], 1).astype(int)
+                            )
+            center = tuple(center)
+            RADIUS = max(int((np.random.normal(0, radius_std, 1) + radius_mean)[0]),2)
+            new = cv2.circle(img, center, RADIUS, 1, -1)
+        if mode == "square":
+            print("Not done yet!")
+            break
+        if mode == "triangle":
+            print("Not done yet!")
+            break
         if mode == "pass":
             new = img
         uuid_str = uuid.uuid4().hex
         cv2.imwrite(output_folder + uuid_str + '.jpg', new*255)
 
 
+
+
 generate_random_images(
         mode = "line",
         output_folder="C:/LV_CHAO_IMAGE/simulation_data/line_200/",
-        N = 1000,
-        length = 10,
-        scale = (200,200)
+        N = 500,
+        length_mean = 10,
+        thickness = 3,
+        scale = (256,256)
+)
+
+generate_random_images(
+        mode = "circle",
+        N = 500,
+        radius_mean = 6,
+        radius_std = 4,
+        output_folder="C:/LV_CHAO_IMAGE/simulation_data/circle_200/",
+        scale = (256,256)
 )
 
 generate_random_images(
         mode = "pass",
-        N = 1000,
+        N = 500,
         output_folder="C:/LV_CHAO_IMAGE/simulation_data/pass_200/",
+        scale = (256,256)
 )
-        
-
-
 
